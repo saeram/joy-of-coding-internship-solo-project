5fc22c1ab19b0e9326c2e4b7789bb719d027204f
Paul Evensen <seram1427@gmail.com>
2024-06-27 22:50:44 +0900

    Handle form submission

--- app/Components/Sidebar/addTask.tsx
+++ app/Components/Sidebar/addTask.tsx
@@ -3,9 +3,11 @@ import { PlusIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
 import React from 'react'
 
+
 const AddTask = () => {
+
   return (
-    <Button asChild className="bg-blue-700 mb-10">
+    <Button asChild className="mb-10">
     <Link href="/tasks/new"><PlusIcon className="mr-2"/>Add Task</Link>
   </Button>
   )
--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,17 +1,79 @@
-import React from 'react'
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from '@/components/ui/button'
+'use client';
 
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import axios from 'axios';
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(50),
+  description: z.string().min(1),
+});
 
 const NewTaskPage = () => {
-  return (
-    <div className="m-10">
-   <Input placeholder='title' className="mb-5" />
-   <Textarea placeholder="description.."/>
-   <Button className="bg-blue-700 mt-5">Submit</Button>
-   </div>
-  )
-}
+   const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
 
-export default NewTaskPage
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    await axios.post('/api/tasks', values);
+    router.push('/');
+  }
+
+  return (
+    <div className="m-10 w-96">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="mt-6">
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                <Textarea placeholder="description.." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="mt-5" type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default NewTaskPage;
