ea6a359d29fc0ae04b30d69449e51542c90ba8e2
Paul Evensen <seram1427@gmail.com>
2024-07-02 10:40:46 +0900

    organizing

--- app/Components/Sidebar/menu.ts
+++ app/Components/Sidebar/menu.ts
@@ -1,10 +1,9 @@
-
 import { CheckIcon, FileTextIcon, HomeIcon } from "@radix-ui/react-icons";
 
 const menu = [
-    { id: 1, label: "All Tasks", icon: HomeIcon, href: "/tasks" },
-    { id: 2, label: "In Progress", icon: FileTextIcon, href: "/inprogress" },
-    { id: 3, label: "Completed", icon: CheckIcon, href: "/completed"},
+  { id: 1, label: "All Tasks", icon: HomeIcon, href: "/tasks" },
+  { id: 2, label: "In Progress", icon: FileTextIcon, href: "/inprogress" },
+  { id: 3, label: "Completed", icon: CheckIcon, href: "/completed" },
 ];
 
 export default menu;
